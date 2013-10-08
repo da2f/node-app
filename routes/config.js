72666eba842a7e82574cabafe9cafdcee22aa1cf
dmitri.frolof@gmail.com <dmitri.frolof@gmail.com>
2013-10-09 00:13:57 +0400

    router: read inner dirs

--- routes/config.js
+++ routes/config.js
@@ -2,14 +2,51 @@ var fs = require('fs');
 var path = require('path');
 var util = require('util');
 
-module.exports = function (app, callback) {
-  fs.readdir(path.join(__dirname), function (err, files) {
+function fn (dir, items, callback, o) {
+  if (!o) {
+    o = {
+      to: null,
+      success: function () {
+        callback(null, items);
+      }
+    };
+  }
+  dir = path.join(dir);
+  fs.readdir(dir, function (err, files) {
+    clearTimeout(o.to);
+    if (err) {
+      return callback(err)
+    }
     files.forEach(function (file) {
+      file = path.join(dir, file);
+      fs.stat(file, function (err, stats) {
+        if (err) {
+          return callback(err)
+        }
+        stats.isFile() && items.push(file);
+        stats.isDirectory() && fn(file, items, callback, o);
+      })
+    });
+    o.to = setTimeout(o.success, 0);
+  });
+}
 
-      if (module.filename === path.join(__dirname, file)) {
+function readdirext (dir, callback) {
+  fn(dir, [], callback);
+}
+
+module.exports = function (app, callback) {
+  readdirext(__dirname, function (err, files) {
+    if (err) {
+      return callback(err)
+    }
+
+    files.forEach(function (file) {
+      if (module.filename === file || file.substr(-3) !== '.js') {
         return undefined;
       }
-      var routes = require(path.join(__dirname, file));
+
+      var routes = require(file);
       routes = !util.isArray(routes) ? [routes] : routes;
       for (var ind = 0, len = routes.length; ind < len; ind++) {
         var route = routes[ind];
