23d78deda3365d6a00dd166eddfae3d71877beae
dmitri.frolof@gmail.com <dmitri.frolof@gmail.com>
2013-10-15 19:43:13 +0400

    module route

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ if ('development' == app.get('env')) {
   swig.setDefaults({cache: false});
 }
 
-require('./routes/config')(app);
+require('ex-route')(app, {
+    src: path.join(__dirname, '/routes')
+});
 
 require('http').createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
